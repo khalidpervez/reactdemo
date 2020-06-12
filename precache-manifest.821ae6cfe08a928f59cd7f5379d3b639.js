473bbef4582894f118673ea7c535d8b7815ed990
khalidpervez <66238854+khalidpervez@users.noreply.github.com>
2020-06-12 21:32:31 +0500

    Updates

---
+++ precache-manifest.821ae6cfe08a928f59cd7f5379d3b639.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd4b934968df19cb2becebb84421b248",
+    "url": "/reactdemo/index.html"
+  },
+  {
+    "revision": "29609ffa7c293097fc23",
+    "url": "/reactdemo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a38c04a4d84d20b5866f",
+    "url": "/reactdemo/static/css/main.01b54bab.chunk.css"
+  },
+  {
+    "revision": "29609ffa7c293097fc23",
+    "url": "/reactdemo/static/js/2.77b0def0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactdemo/static/js/2.77b0def0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a38c04a4d84d20b5866f",
+    "url": "/reactdemo/static/js/main.f5b8ac23.chunk.js"
+  },
+  {
+    "revision": "8d84c5c3313e1f1fd336",
+    "url": "/reactdemo/static/js/runtime-main.9f7c248b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactdemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
